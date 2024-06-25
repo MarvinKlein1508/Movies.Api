1018a15e809cbf11809453da5d0b5e30ebb0153d
MarvinKlein1508 <support@marvinkleinmusic.de>
2024-06-25 13:24:13 +0200

    Implement multiple auth

---
+++ Movies.Api/Auth/AdminAuthRequirement.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Movies.Api.Auth;
+
+public class AdminAuthRequirement : IAuthorizationHandler, IAuthorizationRequirement
+{
+    private readonly string _apiKey;
+
+    public AdminAuthRequirement(string apiKey)
+    {
+        _apiKey = apiKey;
+    }
+    public Task HandleAsync(AuthorizationHandlerContext context)
+    {
+        if (context.User.HasClaim(AuthConstants.AdminUserClaimName, "true"))
+        {
+            context.Succeed(this);
+            return Task.CompletedTask;
+        }
+
+        var httpContext = context.Resource as HttpContext;
+        if (httpContext is null)
+        {
+            return Task.CompletedTask;
+        }
+
+        if (!httpContext.Request.Headers.TryGetValue(AuthConstants.ApiKeyHeaderName, out var extractedApiKey))
+        {
+            context.Fail();
+            return Task.CompletedTask;
+        }
+
+
+        if (_apiKey != extractedApiKey)
+        {
+            context.Fail();
+            return Task.CompletedTask;
+        }
+
+        var identity = (ClaimsIdentity)httpContext.User.Identity!;
+
+        // This GUID is tied to the api key user id. In a real app this information would come from some sort of database
+        identity.AddClaim(new Claim("userId", Guid.Parse("632237e0-7370-4b37-b3ea-11126c85da5e").ToString()));
+        context.Succeed(this);
+    }
+}
