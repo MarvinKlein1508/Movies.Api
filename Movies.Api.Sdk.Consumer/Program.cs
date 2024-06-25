26ed9e530d2938050877f52e191597b93122d4ad
MarvinKlein1508 <support@marvinkleinmusic.de>
2024-06-25 13:42:39 +0200

    Using HttpClientFactory

--- Movies.Api.Sdk.Consumer/Program.cs
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -1,10 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.DependencyInjection;
 using Movies.Api.Sdk;
 using Movies.Contracts.Requests;
 using Refit;
 using System.Text.Json;
 
-var moviesApi = RestService.For<IMoviesApi>("https://localhost:5001");
+//var moviesApi = RestService.For<IMoviesApi>("https://localhost:5001");
+
+var services = new ServiceCollection();
+services.AddRefitClient<IMoviesApi>()
+    .ConfigureHttpClient(x =>
+    {
+        x.BaseAddress = new Uri("https://localhost:5001");
+    })
+    ;
+var provider = services.BuildServiceProvider();
+
+var moviesApi = provider.GetRequiredService<IMoviesApi>();  
 
 var movie = await moviesApi.GetMovieAsync("mark-of-zorro-the-1940");
 
